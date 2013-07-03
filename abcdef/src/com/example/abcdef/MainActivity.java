17951cd238b88acf71b35c9af348744b550cb1bb
SSR-sun <SSR@SSR-PC>
2013-07-03 12:56:44 +0900

    make Toast

--- abcdef/src/com/example/abcdef/MainActivity.java
+++ abcdef/src/com/example/abcdef/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.abcdef;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnTouchListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		View vm = new View(this);
+        vm.setOnTouchListener(this);
+        setContentView(vm);
 	}
 
 	@Override
@@ -18,5 +29,24 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+		if( event.getAction() == MotionEvent.ACTION_DOWN) 
+		{
+			float x = event.getX();
+            float y = event.getY();
+            Toast toastView = Toast.makeText(getApplicationContext(), "x = " + x + "y = " + y, Toast.LENGTH_LONG);
+            toastView.setGravity(Gravity.CENTER, 10, 10);
+            toastView.show();
+			
+			return true;
+		}
+			
+		return false;
+	}
+
+
 
 }
